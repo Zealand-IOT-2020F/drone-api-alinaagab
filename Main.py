9484302e5e318dac4826d6e65d7c6b55cc3ce40a
alinaagab <gabrielas_9952@yahoo.com>
2020-03-20 11:49:51 +0100

    Code

--- Main.py
+++ Main.py
@@ -1,2 +1,28 @@
-
+import sys
+from Drone import Drone
+import time
 #here you should interact with the drone
+ 
+drone = Drone("192.168.10.1",8889)
+drone.connect()
+drone.takeOff()
+time.sleep(1)
+i = 0
+while (drone.battery() > 10 & i<4) :
+    drone.forward(50)
+    time.sleep(1)
+    drone.right(50)
+    time.sleep(1)
+    drone.cw(180)
+    time.sleep(1)
+    drone.forward(50)
+    time.sleep(1)
+    drone.ccw(270)
+    time.sleep(1)
+    drone.forward(50)
+    time.sleep(1)
+    drone.cw(90)
+    i +=1
+
+drone.land()
+drone.end()
